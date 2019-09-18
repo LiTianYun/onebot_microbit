ea3653c510d2dc6585cbbbc339c8aa727619061c
li <851285812@qq.com>
2019-09-18 15:17:34 +0800

    删除重新上传

--- ONEBOT/test.ts
+++
@@ -1,6 +0,0 @@
-// tests go here; this will not be compiled when this package is used as a library
-led.enable(false)
-basic.forever(function () {
-    basic.pause(100)
-    pins.digitalWritePin(DigitalPin.P6, key.readkey())
-})
